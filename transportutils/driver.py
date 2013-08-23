384a1970419278682fcc6505b853c3a9dd2774e9
Philippe Tartaise <ptartaise@dynamikdev.com>
2013-08-23 15:34:42 +0200

    Initial commit

---
+++ transportutils/driver.py
@@ -0,0 +1,87 @@
+"""
+
+"""
+from datetime import datetime, timedelta
+from dateutil import rrule
+
+import pytz
+# GMT = pytz.timezone('UTC')
+# fr = pytz.timezone('Europe/Paris')
+
+ENDNIGHT = 6
+STARTNIGHT = 21
+
+class DriverDaysDates(object):
+    """
+    """
+    def __init__(self,startOfDay,endOfDay):
+        self.startOfDay = startOfDay
+        self.endOfDay = endOfDay
+        self.daytimedelta = self.nighttimedelta = timedelta()
+        self.change = list(rrule.rrule(rrule.DAILY,
+            byhour=(ENDNIGHT,STARTNIGHT),
+            byminute=0,
+            bysecond=0,
+            dtstart=startOfDay,
+            until=endOfDay))
+        if len(list(self.change))==0 :
+            #there no changing type
+            if len(list(rrule.rrule(rrule.DAILY,
+            byhour=0,
+            byminute=0,
+            bysecond=0,
+            dtstart=self.datedeb,
+            until=self.datefin)))>0 or self.startOfDay.hour> STARTNIGHT or self.startOfDay.hour> ENDNIGHT :
+                #there is midnight or start is in night so everything is nigth
+                self.nighttimedelta = abs(self.endOfDay -self.startOfDay)
+                self.daytimedelta = timedelta()
+            else:
+                #overwise is a day
+                self.nighttimedelta = timedelta()
+                self.daytimedelta = abs(self.endOfDay -self.startOfDay)
+        else:
+            self.calcthedelta()
+        
+
+    def calcthedelta(self):
+        lstdate = [self.startOfDay] + list(self.change) + [self.endOfDay]
+        print lstdate
+        for k in range(1,len(lstdate)):
+            print k,lstdate[k-1],lstdate[k]
+            isNight = False
+            if lstdate[k-1] in self.change: #start from a change
+                if lstdate[k-1].hour == STARTNIGHT:
+                    isNight = True
+            if lstdate[k] in self.change: #start from a change
+                if lstdate[k].hour == ENDNIGHT:
+                    isNight = True
+            if isNight:
+                self.nighttimedelta += abs(lstdate[k] - lstdate[k-1])
+            else:
+                self.daytimedelta += abs(lstdate[k] - lstdate[k-1])
+
+
+class DriverDates(object):
+    """
+    """
+    DriverTimeZone=pytz.timezone('Europe/Paris')
+    def __init__(self,datedeb,datefin):
+        self.datedeb= datedeb.astimezone(self.DriverTimeZone)
+        self.datefin= datefin.astimezone(self.DriverTimeZone)
+        lstdate =[self.datedeb] +\
+         list(rrule.rrule(rrule.DAILY,
+            byhour=0,
+            byminute=0,
+            bysecond=0,
+            dtstart=self.datedeb,
+            until=self.datefin)) +\
+             [self.datefin]
+        self.days = [ DriverDaysDates(lstdate[k-1],lstdate[k]) for k in range(1,len(lstdate))]    
+
+if __name__ == '__main__':
+    GMT = pytz.timezone('UTC')
+    dd = DriverDates(datetime.now(GMT) - timedelta(days=3), datetime.now(GMT))
+    for day in dd.days:
+        print day.startOfDay,"---", day.endOfDay
+        print "night : ",day.nighttimedelta
+        print "day : ",day.daytimedelta
